e009abac6c1a467614f83f02920dfef1a8074f55
Terence Svubure <tsvubure@gmail.com>
2023-05-14 19:04:17 +0200

    week first commit

---
+++ dayWeek.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+  int number;
+  
+  cout << "Enter a number between 1 and 7 and I will display the day: " << endl;
+
+  cin >> number;
+
+  switch(number){
+    case 1:
+    cout << "Monday" << endl;
+    break;
+    
+    case 2:
+    cout << "Tuesday" << endl;
+    break;
+    
+    case 3:
+    cout << "Wednesday" << endl;
+    break;
+    
+    case 4:
+    cout << "Thursday" << endl;
+    break;
+    
+    case 5:
+    cout << "Friday" << endl;
+    break;
+    
+    case 6:
+    cout << "Saturday" << endl;
+    break;
+    
+    case 7:
+    cout << "Sunday" << endl;
+    break;
+    
+    default:
+    cout << "Invalid input" << endl;
+    break;
+  }
+
+  return 0;
+}
