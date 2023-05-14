99af39a6431122557ecec541d5bc88d119b88405
Terence Svubure <tsvubure@gmail.com>
2023-05-14 09:03:48 +0200

    second commit

---
+++ findPerson.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int salary1 = 56000;
+
+  int salary2 = 89000;
+
+  if (salary1 > salary2) {
+    cout << "Person1 has the highest salary" << endl;
+  }else {
+    cout << "Person2 has the highest salary" << endl;
+  }
+
+  return 0;
+}
